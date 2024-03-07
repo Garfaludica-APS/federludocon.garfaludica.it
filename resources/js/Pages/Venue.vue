0522b21d19bfcf33ba755a1b6d890db276d5a92a
Niccolò Scatena <speedjack95@gmail.com>
2024-03-07 23:39:29 +0100

    Add pages

---
+++ resources/js/Pages/Venue.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+	<AppLayout :title="$t('Venue')">
+		<div class="py-12">
+			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 animate-fade-in">
+				<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+					<Welcome />
+				</div>
+			</div>
+		</div>
+	</AppLayout>
+</template>
