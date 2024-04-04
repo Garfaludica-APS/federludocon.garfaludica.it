e1fcfc69b853ae858e489dda4adc67d1243f4b9c
MarcoPacciani <47499209+MarcoPacciani@users.noreply.github.com>
2024-04-04 23:26:20 +0200

    Aggiunto parte della traduzione

--- resources/views/mail/markdown/admin-invitation.blade.php
+++ resources/views/mail/markdown/admin-invitation.blade.php
@@ -1,15 +1,19 @@
 <x-mail::message :message="$message" :logoPath="$logoPath">
 
-{{__('You have been invited by :username (:email) to manage the following hotels:',['username'=> $invitation->creator->username, 'email' => $invitation->creator->email])}}
-@foreach ($hotels as $hotel)
-	<ul>{{ $hotel->name }}</ul>
-@endforeach
-
-For the event GobCon 2024 Garfagnana.
+# {{__('Email sent by :username (:email)',['username'=> $invitation->creator->username, 'email' => $invitation->creator->email])}}<br>
+{{__('Hello, with this email, we invite you to manage the following hotels:')}}
+<ul>
+    @foreach ($hotels as $hotel)
+	<li><i><b>{{__(':hotel', ['hotel' => $hotel->name]) }}</b></i></li>
+    @endforeach
+</ul>
+{{__('for the event GobCon 2024 Garfagnana.')}}
 <x-mail::button :url="$invitationUrl">
 Accept Invitation
 </x-mail::button>
-
+<b>{{__('After 3 days the invitation link expires and will no longer work.')}}</b>
+<br>
+{{__('We hope that this event will be profitable.')}}<br>
 Thanks,<br>
 Garfaludica APS
 </x-mail::message>
