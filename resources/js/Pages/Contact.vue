86069487f8f27cbc458501e0f99b766640e7df6b
MarcoPacciani <47499209+MarcoPacciani@users.noreply.github.com>
2024-04-10 18:58:03 +0200

    aggiustamento pagina contact e info

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -25,17 +25,17 @@ const emailAddressHref = 'mailto:' + emailAddress;
 				<div>
 					<div class="p-6 lg:p-8 bg-slate-100 dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
 						<ApplicationLogo class="block h-16 w-auto" />
-						<div class="mt-6 text-black dark:text-slate-200 text-lg">
-							<p v-html="$t('Garfa_email_intro')"></p>
-							<a class="font-bold hover:bg-gradient-to-r to-[#0b8a26] from-[#9bc009] via-[#fbdb19] hover:inline-block hover:text-transparent hover:bg-clip-text" rel="noopener" href="mailto:info@garfaludica.it" target="_blank">info@garfaludica.it</a> |
-							<a class="font-bold hover:bg-gradient-to-r to-[#0b8a26] from-[#9bc009] via-[#fbdb19] hover:inline-block hover:text-transparent hover:bg-clip-text" rel="noopener" href="mailto:garfaludica@pec.it" target="_blank">garfaludica@pec.it</a>
-							<p class="mt-3" v-html="$t('Garfa_management_intro')"></p>
+						<div class="mt-4 text-black dark:text-slate-200 text-lg">
+							<p v-html="$t('Garfa_email_exp')"></p>
+							<p class="text inline" v-html="$t('Garfa_email_intro')"></p>
+							<a class="font-bold hover:bg-gradient-to-r to-[#0b8a26] from-[#9bc009] via-[#fbdb19] hover:inline-block hover:text-transparent hover:bg-clip-text" rel="noopener" href="mailto:info@garfaludica.it" target="_blank">info@garfaludica.it</a>
+							<p class="mt-3" v-html="$t('Garfa_management_intro')"></p><br>
 							<div class="font-bold text inline">{{ $t('Presidency') }}</div>: <a class="hover:text-indigo-500" rel="noopener" href="tel:+393247460610" target="_blank">324-746-0610</a> (<a class="hover:underline hover:decoration-indigo-600" rel="external noopener" href="https://t.me/Pillian" target="_blank">Christian Pilli</a>)<br>
 							<div class="font-bold text inline">{{ $t('Secretariate') }}</div>: <a class="hover:text-indigo-500" rel="noopener" href="tel:+393314432124" target="_blank">331-443-2124</a> (<a class="hover:underline hover:decoration-indigo-600" rel="external noopener" href="https://t.me/SpeedJack" target="_blank">Niccolò Scatena</a>)<br>
 							<div class="font-bold text inline">{{ $t('Vice presidency') }}</div>: <a class="hover:text-indigo-500" rel="noopener" href="tel:+393282228615" target="_blank">328-222-8615</a> (<a class="hover:underline hover:decoration-indigo-600" rel="external noopener" href="https://t.me/Badpinger" target="_blank">Simone Bertagni</a>)<br>
 							<div class="font-bold text inline">{{ $t('Counselors') }}</div>: <a class="hover:underline hover:decoration-indigo-600" rel="external noopener" href="https://t.me/CannibalSmit" target="_blank">Riccardo Evangelisti</a>, <a class="hover:underline hover:decoration-indigo-600" rel="external noopener" href="https://t.me/Picarus" target="_blank">Pierluigi Pellizzer</a><br>
 							<div>
-								<a href="https://t.me/associazionegarfaludica" type="button" class="inline-flex items-center justify-center h-8 px-6 py-6 border-2 rounded-full font-semibold uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 bg-gray-500 dark:bg-gray-900/20 text-white text-sm border-indigo-500 hover:border-white hover:bg-indigo-500 active:bg-indigo-700 focus:ring-indigo-500 mx-4 my-4" target="_blank">
+								<a href="https://t.me/gobcongarfagnana" type="button" class="inline-flex items-center justify-center h-8 px-6 py-6 border-2 rounded-full font-semibold uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 bg-gray-500 dark:bg-gray-900/20 text-white text-sm border-indigo-500 hover:border-white hover:bg-indigo-500 active:bg-indigo-700 focus:ring-indigo-500 mx-4 my-4" target="_blank">
 									<p class="text-xl"> {{ $t('Telegram Group') }}</p>
 								</a>
 							</div>
@@ -49,9 +49,7 @@ const emailAddressHref = 'mailto:' + emailAddress;
 									{{ $t('Structures') }}
 								</h1>
 							</div>
-							<p class="mt-6 text-black dark:text-slate-200 text-lg">
-								{{ $t('Structures_info') }}
-							</p>
+							<p class="mt-6 text-black dark:text-slate-200 text-lg" v-html="$t('Structures_info')"></p>
 							<div>
 								<p class="mt-6 text-black dark:text-slate-200 text-2xl font-bold italic text-[#e42d33]">
 									{{ $t('hotel_name_Panoramic Hotel') }}
