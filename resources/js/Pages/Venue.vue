2283972b101e9e4f6ede97c3f146181ab72022d1
Niccolò Scatena <speedjack95@gmail.com>
2024-04-10 17:50:08 +0200

    Add map

--- resources/js/Pages/Venue.vue
+++ resources/js/Pages/Venue.vue
@@ -8,14 +8,16 @@ export default {
 </script>
 
 <script setup>
-import Welcome from '@/Components/Welcome.vue';
+import VenueMap from '@/Components/VenueMap.vue';
 </script>
 
 <template>
 	<div class="py-12">
-		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 animate-fade-in">
-			<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-				<Welcome />
+		<div class="w-screen max-w-7xl mx-auto sm:px-6 lg:px-8 animate-fade-in">
+			<div class="w-full bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+				<div class="min-w-full max-w-full mx-auto">
+					<VenueMap />
+				</div>
 			</div>
 		</div>
 	</div>
