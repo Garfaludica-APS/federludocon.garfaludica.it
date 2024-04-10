b7dbcd6fe6ea5f0aedc99aa81ce1bc1f33ab25e4
MarcoPacciani <47499209+MarcoPacciani@users.noreply.github.com>
2024-04-10 17:37:34 +0200

    traduzione pagine lang/it

---
+++ lang/it/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright © 2024 - Garfaludica APS - MIT License
+ */
+
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used during authentication for various
+	| messages that we need to display to the user. You are free to modify
+	| these language lines according to your application's requirements.
+	|
+	 */
+
+	'failed' => 'Queste credenziali non corrispondono a nessun account',
+	'password' => 'La password inserita non è corretta.',
+	'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
+];
---
+++ lang/it/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright © 2024 - Garfaludica APS - MIT License
+ */
+
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reset Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	 */
+
+	'reset' => 'La tua password è stata resettata.',
+	'sent' => 'Ti abbiamo inviato via email il link per reimpostare la password.',
+	'throttled' => 'Si prega di attendere prima di riprovare.',
+	'token' => 'Questo token per reimpostare la password non è valido.',
+	'user' => "Non riusciamo a trovare un utente con quell'indirizzo email.",
+];
