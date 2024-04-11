75e42edbf40cc17615e21cf3eb32fe42a9e74933
Niccolò Scatena <speedjack95@gmail.com>
2024-04-11 12:39:13 +0200

    Fix npm run build

--- lang/it/stored.php
+++ lang/it/stored.php
@@ -7,36 +7,34 @@ declare(strict_types=1);
  */
 
 return [
-	'hotel_Panoramic Hotel_presentation_text' => <<<'EOD'
-		L'Hotel Panoramico situato nel cuore della Garfagnana, accoglie i suoi ospiti con un ampio e curato giardino dotato di piscina riscaldata, per un soggiorno di totale relax. Un'ampia sala comune e il bar offrono la possibilità di momenti di socializzazione; il ristorante fino a 150 coperti offre un'ottima cucina ricca di piatti della tradizione garfagnina.<br>
-		Situata in un punto strategico e grazia alla vicinanza con le riserve naturali diventa un punto di sosta per gli amanti della natura che possono avventurarsi in escursioni a piedi, a cavallo o in mountain-bike. Parcheggio disponibile di fronte all'Hotel.<br><br>
-		Durante la GobCon 2024, l'Hotel metterà a disposizione i suoi spazi per far continuare a giocare i partecipanti <strong style="color:red"><u>tutta la notte</u></strong>.<br><br>
+	'hotel_Panoramic Hotel_presentation_text' => 'L\'Hotel Panoramico situato nel cuore della Garfagnana, accoglie i suoi ospiti con un ampio e curato giardino dotato di piscina riscaldata, per un soggiorno di totale relax. Un\'ampia sala comune e il bar offrono la possibilità di momenti di socializzazione; il ristorante fino a 150 coperti offre un\'ottima cucina ricca di piatti della tradizione garfagnina.<br>
+Situata in un punto strategico e grazia alla vicinanza con le riserve naturali diventa un punto di sosta per gli amanti della natura che possono avventurarsi in escursioni a piedi, a cavallo o in mountain-bike. Parcheggio disponibile di fronte all\'Hotel.<br><br>
+Durante la GobCon 2024, l\'Hotel metterà a disposizione i suoi spazi per far continuare a giocare i partecipanti <strong style="color:red"><u>tutta la notte</u></strong>.<br><br>
 
-		L'Hotel mette a disposizione camere <strong style="color:green"><u>matrimoniali</u></strong>, <strong style="color:green"><u>doppie</u></strong>, <strong style="color:green"><u>triple</u></strong> e <strong style="color:green"><u>quadruple</u></strong>. Per un dettaglio sui prezzi, vedere la pagina <strong>Info</strong>.<br><br>
+L\'Hotel mette a disposizione camere <strong style="color:green"><u>matrimoniali</u></strong>, <strong style="color:green"><u>doppie</u></strong>, <strong style="color:green"><u>triple</u></strong> e <strong style="color:green"><u>quadruple</u></strong>. Per un dettaglio sui prezzi, vedere la pagina <strong>Info</strong>.<br><br>
 
-		Sito web dell'Hotel: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://www.albergopanoramico.it" target="_blank">www.albergopanoramico.it</a><br>
-		Indicazioni per raggiungere l'Hotel: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/2XB6r4VUTRcX21mD9" target="_blank">Google Maps</a>
-		EOD,
+Sito web dell\'Hotel: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://www.albergopanoramico.it" target="_blank">www.albergopanoramico.it</a><br>
+Indicazioni per raggiungere l\'Hotel: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/2XB6r4VUTRcX21mD9" target="_blank">Google Maps</a>',
 
-	'hotel_Isera Refuge_presentation_text' => <<<'EOD'
-		Il Rifugio Isera Orecchiella si trova in Garfagnana, alto appennino lucchese, all'interno del Parco nazionale dell'Appennino tosco-emiliano e al confine con la Riserva Naturale Statale dell'Orecchiella.<br>
-		Situato ai piedi della Pania di Corfino, è raggiungibile in macchine e dista circa 30 minuti da Castelnuovo di Garfagnana.<br>
-		Dal Rifugio partono i principali sentieri che portano alla scoperta della Pania, ed alcune semplici passeggiate che conducono al Giardino Botanico "Maria Ansaldi - Pania di Corfino" e al Centro Visitatori della Riserva Naturale Statale dell'Orecchiella.<br>
-		Il caratteristico borgo di Corfino si trova a pochi chilometri dal Rifugio e può essere raggiunto tramite un sentiero che passa attraverso il suggestivo alpeggio di Pruno.<br><br>
-		Durante la Gobcon 2024, il Rifugio Isera metterà a disposizione i suoi spazi esterni per allestire le <strong style="color:red"><u>due aree di gioco</u></strong>, dove i partecipanti potranno giocare e divertirsi tra di loro fino all'ora di cena.<br><br>
 
-		Il Rifugio ha a disposizione un'<strong style="color:green"><u>unica camerata condivisa</u></strong> con <strong style="color:green"><u>12 posti letto a castello</u></strong>. In alternativa, è possibile pernottare in tenda nell'ampio prato recintato e attrezzato che circonda il Rifugio. Per un dettaglio sui prezzi, vedere la pagina <strong>Info</strong>.<br><br>
 
-		Sito web del Rifugio: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="http://www.rifugioisera.com" target="_blank">www.rifugioisera.com</a><br>
-		Indicazioni per raggiungere il Rifugio: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/86ecLf95QachVthGA" target="_blank">Google Maps</a>
-		EOD,
+	'hotel_Isera Refuge_presentation_text' => 'Il Rifugio Isera Orecchiella si trova in Garfagnana, alto appennino lucchese, all\'interno del Parco nazionale dell\'Appennino tosco-emiliano e al confine con la Riserva Naturale Statale dell\'Orecchiella.<br>
+Situato ai piedi della Pania di Corfino, è raggiungibile in macchine e dista circa 30 minuti da Castelnuovo di Garfagnana.<br>
+Dal Rifugio partono i principali sentieri che portano alla scoperta della Pania, ed alcune semplici passeggiate che conducono al Giardino Botanico "Maria Ansaldi - Pania di Corfino" e al Centro Visitatori della Riserva Naturale Statale dell\'Orecchiella.<br>
+Il caratteristico borgo di Corfino si trova a pochi chilometri dal Rifugio e può essere raggiunto tramite un sentiero che passa attraverso il suggestivo alpeggio di Pruno.<br><br>
+Durante la Gobcon 2024, il Rifugio Isera metterà a disposizione i suoi spazi esterni per allestire le <strong style="color:red"><u>due aree di gioco</u></strong>, dove i partecipanti potranno giocare e divertirsi tra di loro fino all\'ora di cena.<br><br>
 
-	'hotel_Braccicorti Farmhouse_presentation_text' => <<<'EOD'
-		Un agriturismo in un tradizionale e solare podere della Garfagnana, a pochi passi da Lucca, in una vallata ricca di natura tra i monti del Parco nazionale dell'Appennino tosco-emiliano e le Alpi Apuane. Fortezze, borghi storici, vette e laghi caratterizzano questo angolo segreto dell'alta Toscana, visitabili a piedi o in bicicletta su un'ampia rete di percorsi, anche partendo dall'agriturismo, immpergendosi nelle suggestioni e nei tanti sapori tradizionali. E i vostri animali sono i benvenuti!<br><br>
+Il Rifugio ha a disposizione un\'<strong style="color:green"><u>unica camerata condivisa</u></strong> con <strong style="color:green"><u>12 posti letto a castello</u></strong>. In alternativa, è possibile pernottare in tenda nell\'ampio prato recintato e attrezzato che circonda il Rifugio. Per un dettaglio sui prezzi, vedere la pagina <strong>Info</strong>.<br><br>
 
-		L'Agriturismo mette a dispozione <strong style="color:green"><u>SOLTANTO UNA camera per due persone</u></strong>, compresiva di colazione. L'Agriturismo è situato a circa 20/25 minuti di macchina dal luogo dell'evento.<br><br>
+Sito web del Rifugio: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="http://www.rifugioisera.com" target="_blank">www.rifugioisera.com</a><br>
+Indicazioni per raggiungere il Rifugio: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/86ecLf95QachVthGA" target="_blank">Google Maps</a>',
 
-		Sito web dell'Agriturismo: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://www.braccicorti.it" target="_blank">www.braccicorti.it</a><br>
-		Indicazioni per raggiungere l'Agriturismo: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/PRGAVGDyoGNdQ96N7" target="_blank">Google Maps</a>
-		EOD,
+
+
+'hotel_Braccicorti Farmhouse_presentation_text' => 'Un agriturismo in un tradizionale e solare podere della Garfagnana, a pochi passi da Lucca, in una vallata ricca di natura tra i monti del Parco nazionale dell\'Appennino tosco-emiliano e le Alpi Apuane. Fortezze, borghi storici, vette e laghi caratterizzano questo angolo segreto dell\'alta Toscana, visitabili a piedi o in bicicletta su un\'ampia rete di percorsi, anche partendo dall\'agriturismo, immpergendosi nelle suggestioni e nei tanti sapori tradizionali. E i vostri animali sono i benvenuti!<br><br>
+
+L\'Agriturismo mette a dispozione <strong style="color:green"><u>SOLTANTO UNA camera per due persone</u></strong>, compresiva di colazione. L\'Agriturismo è situato a circa 20/25 minuti di macchina dal luogo dell\'evento.<br><br>
+
+Sito web dell\'Agriturismo: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://www.braccicorti.it" target="_blank">www.braccicorti.it</a><br>
+Indicazioni per raggiungere l\'Agriturismo: <a style="color:rgb(99, 102, 241)" rel="noopener external" href="https://maps.app.goo.gl/PRGAVGDyoGNdQ96N7" target="_blank">Google Maps</a>',
 ];
