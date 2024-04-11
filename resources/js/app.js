036959d37c575b4c6a7dc51efe0dcc85b9698b93
Niccolò Scatena <speedjack95@gmail.com>
2024-04-11 20:22:08 +0200

    Client side hydration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
+import { createSSRApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { modal } from 'inertia-modal';
@@ -15,7 +15,7 @@ createInertiaApp({
 	title: (title) => title ? `${title} - ${appName} - ${orgName}` : `${appName} - ${orgName}`,
 	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
 	setup({ el, App, props, plugin }) {
-		return createApp({ render: () => h(App, props) })
+		return createSSRApp({ render: () => h(App, props) })
 			.use(modal, {
 				resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
 			})
